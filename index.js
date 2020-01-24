1d91710cce5ea693566afa9dfc00111303952a55
bryce <boylebryce@gmail.com>
2020-01-24 23:12:31 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const auth = require('./auth.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(auth.token);
