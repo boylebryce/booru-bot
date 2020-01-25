9b07195beb45a232ac4800a92d1d8157cd2b9b10
bryce <boylebryce@gmail.com>
2020-01-25 04:05:30 +0000

    add image search command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const auth = require('./auth.json');
+const config = require('./config.json');
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,4 +7,28 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(auth.token);
+client.login(config.token);
+
+client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'search') {
+        let searchString = '';
+        args.forEach(element => searchString += element + ' ');
+        fetch(`${config.api}/search.php?search=${searchString}`)
+            .then(response => {
+                return response.json();
+            }).then(response => {
+                if (response.length) {
+                    const index = Math.floor(Math.random() * response.length);
+                    message.channel.send(`${config.images}/${response[index]['img_path']}`)
+                }
+                else {
+                    message.channel.send('Sorry, no results!');
+                }
+            });
+    }
+});
