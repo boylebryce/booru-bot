577089b91c1b0ca7b4d64c3a14f2914699584a0d
bryce <boylebryce@gmail.com>
2020-01-27 01:40:15 +0000

    add cooldown to commands

--- commands/channel-info.js
+++ commands/channel-info.js
@@ -1,9 +1,13 @@
 module.exports = {
     name: 'channel-info',
     description: 'Information about the current channel.',
+    cooldown: 5,
     guildOnly: true,
     argsRequired: false,
     execute(message, args) {
-        message.channel.send(`Channel name: ${message.channel.name}`);
+        message.channel.send(`Channel name: ${message.channel.name}`
+            + `\nChannel type: ${message.channel.type}`
+            + `\nChannel id: ${message.channel.id}`
+            + `\nChannel parent: ${message.channel.parent}`);
     },
 };
